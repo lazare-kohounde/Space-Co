175d8afef1d8ec5e6571a530a38d9b3694fbc006
Lazare <depositemaster@gmail.com>
2025-04-10 12:54:13 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\clientController;
 
 
+// roote client
+
 Route::get('/', function () {    return view('client.index');})->name('accueil');
 Route::get('/log-admin', function () {    return view('admin.index');})->name('dashboard');
 
@@ -11,4 +13,10 @@ Route::get('/salle', [clientController::class, 'liste_salle'])->name('salle');
 Route::get('/salle-details', [clientController::class, 'salledetails'])->name('salledetails');
 Route::get('/contact', [clientController::class, 'contact'])->name('contact');
 
+
+
+
+// roote admin
+
+Route::get('/log-admin', function () {    return view('admin.index');})->name('dashboard');
 
