f738731cf2448085568505c0633e988cac9c20b1
Lazare <depositemaster@gmail.com>
2025-04-10 13:00:09 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\clientController;
 // roote client
 
 Route::get('/', function () {    return view('client.index');})->name('accueil');
-Route::get('/log-admin', function () {    return view('admin.index');})->name('dashboard');
 
 Route::get('/salle', [clientController::class, 'liste_salle'])->name('salle');
 Route::get('/salle-details', [clientController::class, 'salledetails'])->name('salledetails');
